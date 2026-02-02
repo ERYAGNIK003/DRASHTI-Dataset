6ec2b070c0bd150f91958547469228a685fbaca4
ERYAGNIK003 <bhavsaryagnik003@gmail.com>
2026-02-02 18:48:48 +0530

    Update training parameters and dataset path

--- YOLO/DRASHTI-HaOBB_Train_YOLOv8n_obb.py
+++ YOLO/DRASHTI-HaOBB_Train_YOLOv8n_obb.py
@@ -9,9 +9,9 @@ print(f"Using device: {device}")
 
 model = YOLO('yolov8n-obb.yaml')  #Select the model yaml file of your choice like yolov8s-obb.yaml, yolov8m-obb.yaml, yolov8l-obb.yaml, yolov8x-obb.yaml
 model.train(
-    data='DRASTI.yaml', # Give the path of the DRASTI dataset yaml file.
-    epochs=50,
-    imgsz=1280,
+    data='DRASHTI-HaOBB.yaml', # Give the path of the DRASTI dataset yaml file.
+    epochs=30,
+    imgsz=1024,
     batch=64,
     workers=16,
     device=device,
@@ -19,6 +19,6 @@ model.train(
 )
 
 model.val(
-    data='DRASTI.yaml', # Give the path of the DRASTI dataset yaml file.
+    data='DRASHTI-HaOBB.yaml', # Give the path of the DRASTI dataset yaml file.
     project='/train_project_folder' #Give the path of Folder where the validating files are stored.
 )
